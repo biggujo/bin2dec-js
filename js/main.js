5b0e7b25b4f882784be7a9cfe9ee8be0f6e7c535
biggujo <gen.style22@gmail.com>
2023-02-16 16:52:20 +0200

    Added radix selection

--- js/main.js
+++ js/main.js
@@ -1,18 +1,7 @@
-const inputFieldBinRef = document.getElementById("input-bin");
-const outputFieldDecRef = document.getElementById("output-dec");
+const inputFieldBinRef = document.getElementById("input-input");
+const outputFieldDecRef = document.getElementById("output-input");
 
 const btnToCalculateRef = document.getElementById("btn-submit");
-
-const convertBinaryToDecimal = (binaryNumberAsString) => {
-  return binaryNumberAsString
-    .split("")
-    .reduce(
-      (decimalAsAccumulator, currentCharOfBinary, index, originalString) =>
-        (decimalAsAccumulator +=
-          currentCharOfBinary * Math.pow(2, originalString.length - index - 1)),
-      0
-    );
-};
 
 const isStringABinaryNumber = (binaryNumberString) => {
   // * From start to end, find multiple occurrences of 0 and 1
@@ -21,13 +10,85 @@ const isStringABinaryNumber = (binaryNumberString) => {
   return regex.test(binaryNumberString);
 };
 
+const createDropdownWithRowOfNumbersEl = (
+  minNumber,
+  maxNumber,
+  defaultValue,
+  htmlID
+) => {
+  const dropdownListRef = document.createElement("select");
+
+  dropdownListRef.id = htmlID;
+  dropdownListRef.classList.add("dropdown");
+
+  for (let i = minNumber; i <= maxNumber; i++) {
+    const optionRadix = document.createElement("option");
+    optionRadix.textContent = i;
+
+    if (i === defaultValue) {
+      optionRadix.selected = "selected";
+    }
+
+    dropdownListRef.appendChild(optionRadix);
+  }
+  return dropdownListRef;
+};
+
+// * Add dropdowns to choose a number radix
+const minRadix = 2;
+const maxRadix = 16;
+
+const createParagraphEl = (givenTextContent, classArray) => {
+  const paragraphEl = document.createElement("p");
+
+  paragraphEl.classList.add(...classArray);
+  paragraphEl.textContent = givenTextContent;
+
+  return paragraphEl;
+};
+
+const dropdownMenuInputRadixEl = createDropdownWithRowOfNumbersEl(
+  minRadix,
+  maxRadix,
+  2,
+  "input-option-radix"
+);
+
+// * Create and render dropdowns
+inputFieldBinRef.before(
+  createParagraphEl("Radix:", ["text", "text--right-margin"]),
+  dropdownMenuInputRadixEl
+);
+
+const dropdownMenuOutputRadixEl = createDropdownWithRowOfNumbersEl(
+  minRadix,
+  maxRadix,
+  10,
+  "input-option-radix"
+);
+
+outputFieldDecRef.before(
+  createParagraphEl("Radix:", ["text", "text--right-margin"]),
+  dropdownMenuOutputRadixEl
+);
+
+// * Add calculation on click
 btnToCalculateRef.addEventListener("click", () => {
-  const binaryValueFromInput = inputFieldBinRef.value;
+  const inputValue = inputFieldBinRef.value;
+  const radixInput = Number(dropdownMenuInputRadixEl.value);
+  const radixOutput = Number(dropdownMenuOutputRadixEl.value);
 
-  // * If there correct decimal value
-  if (isStringABinaryNumber(binaryValueFromInput)) {
-    const decimalValueConverted = convertBinaryToDecimal(binaryValueFromInput);
+  let result = 0;
 
-    outputFieldDecRef.value = decimalValueConverted;
-  } else alert("Only such symbols are allowed: 0, 1");
+  if (radixInput === radixOutput) {
+    outputFieldDecRef.value = inputValue;
+    return;
+  }
+
+  if (radixInput > 10) {
+    result = parseInt(inputValue, radixInput);
+    outputFieldDecRef.value = result.toString(radixOutput);
+  } else {
+    outputFieldDecRef.value = inputValue.toString(radixInput);
+  }
 });
